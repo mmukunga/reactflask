f2977e6502ae0d02378a6ad6e8d9f6b3baceca6b
Simon Mukunga <mmukunga@gmail.com>
2021-09-26 22:30:43 +0200

    first commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-
 import React, { useEffect, useState } from 'react';
 import axios from 'axios'
 
@@ -8,36 +7,31 @@ function App() {
   const [getMessage, setGetMessage] = useState({})
 
   useEffect(()=>{
-    axios.get('https://reactflask-smb.herokuapp.com/flask/hello').then(response => {
+    axios.get('https://react-flask-tutorial.herokuapp.com/flask/hello').then(response => {
       console.log("SUCCESS", response)
       setGetMessage(response)
     }).catch(error => {
       console.log(error)
     })
 
-  }, []);
+    // axios.get('http://localhost:5000/flask/hello').then(response => {
+    //   console.log("SUCCESS", response)
+    //   setGetMessage(response)
+    // }).catch(error => {
+    //   console.log(error)
+    // })
 
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <p>React + Flask Tutorial</p>
+        <p>React + Flask Tutorial</p>
         <div>{getMessage.status === 200 ? 
           <h3>{getMessage.data.message}</h3>
           :
           <h3>LOADING</h3>}</div>
+      </header>
     </div>
   );
 }
