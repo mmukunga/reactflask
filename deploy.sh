664cc6a049e3475c907bcb2ba497311019d1e277
Simon Mukunga <mmukunga@gmail.com>
2021-09-29 20:52:54 +0200

    first commit

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 
 #!/bin/bash
 echo "Hello, World!" 
-cd D:\Temps\ReactFlask\frontend
-echo rmdir 'D:\Temps\ReactFlask\frontend\build\'  /s /q
-echo remove-item 'D:\Temps\ReactFlask\frontend\build\*' -recurse -force
-rm -rf 'D:\Temps\ReactFlask\frontend\build'
+cd 'D:/Temps/ReactFlask/frontend'
+echo rmdir 'D:/Temps/ReactFlask/frontend/build'  /s /q
+echo remove-item 'D:/Temps/ReactFlask/frontend/build/*' -recurse -force
+rm -rf 'D:/Temps/ReactFlask/frontend/build'
 npm run build
-cd D:\Temps\ReactFlask
+cd 'D:/Temps/ReactFlask'
 
 git status
 git add .
