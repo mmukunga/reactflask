d22e897b6d7873cd8bffd2dbdc46b74288154594
Simon Mukunga <mmukunga@gmail.com>
2021-10-01 01:01:54 +0200

    first commit

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ def get_recipe(filename):
              file_dict[line].append(line)
              meny_innhold = list()
              found = True
-          if found == True and line in fArray:                       
+          elif found == True and line not in fArray:                       
              print('1.line..' + line)  
              file_dict[line].append(meny_innhold)
              break
