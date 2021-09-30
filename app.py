f5ab45afde3cf2ddc327fa898f862f5e0bde7413
Simon Mukunga <mmukunga@gmail.com>
2021-09-30 22:11:37 +0200

    first commit

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ def get_recipe(filename):
     with open(selected_files[0], 'r', encoding="ISO-8859-1") as f:
         for line in f:
            print(line) 
-           if line not in ["Tittel:", "Undertittel:", "Ingredienser:", "Fremgangsmaate:", "Tips:"]:
+           if line.rstrip() not in ["Tittel:", "Undertittel:", "Ingredienser:", "Fremgangsmaate:", "Tips:"]:
               print('1.line..')  
               meny_innhold.append(line)
            else:
